4b5008831d270c74ce176fbbefd44c5f78f20037
Erick J. Vallejo <erickjvallejo@gmail.com>
2020-09-12 14:16:06 -0500

    Update change of laptop

--- java-spring-cucumber/src/main/java/com/cucumberspring/controller/PostController.java
+++ java-spring-cucumber/src/main/java/com/cucumberspring/controller/PostController.java
@@ -4,7 +4,6 @@ import com.cucumberspring.domain.Post;
 import com.cucumberspring.repository.PostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-public class    PostController {
+public class PostController {
 
     private final Logger log = LoggerFactory.getLogger(PostController.class);
     private PostRepository postRepository;
--- selenium/src/test/java/simpletest/defs/SimpleTestDefs.java
+++ selenium/src/test/java/simpletest/defs/SimpleTestDefs.java
@@ -25,7 +25,7 @@ public class SimpleTestDefs {
     @Before
     public void setUp() {
 
-        System.setProperty("webdriver.chrome.driver", "D:\\Program Files\\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
         driver = new ChromeDriver();
         log.info("Created the ChromeDriver");
     }
